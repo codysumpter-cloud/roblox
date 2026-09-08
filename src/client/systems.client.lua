a827397046db6b7234d0641e67807e977aa6eb17
Prismtek <cody.sumpter@gmail.com>
2026-09-08 11:02:54 -0400

    Start PvP combat controller

--- src/client/systems.client.lua
+++ src/client/systems.client.lua
@@ -2,9 +2,11 @@
 local AdminController = require(script.Parent.AdminController)
 local WorldEventController = require(script.Parent.WorldEventController)
 local GaspHumanoidController = require(script.Parent.GaspHumanoidController)
+local CombatController = require(script.Parent.CombatController)
 
 AdminController.start()
 WorldEventController.start()
 GaspHumanoidController.start()
+CombatController.start()
 
-print("[PocketBuddy] optional systems started: admin, world events, GASP humanoid adapter")
+print("[PocketBuddy] optional systems started: admin, world events, GASP humanoid adapter, PvP combat")
